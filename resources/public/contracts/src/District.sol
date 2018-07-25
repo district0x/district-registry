cdb419e9446aee29b7dbc651bb541fd84953a5cd
Dom Kiva-Meyer <git@domkm.com>
2018-07-25 19:21:13 -0400

    Staking/unstaking works, but the contract is too large Resolves #4

--- resources/public/contracts/src/District.sol
+++ resources/public/contracts/src/District.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
 import "./RegistryEntry.sol"; 
+import "math/Power.sol";
 import "token/stakebank/StakeBank.sol";
 
 /**
@@ -11,12 +12,26 @@ import "token/stakebank/StakeBank.sol";
  * pointing into single intance of it.
  */
 
-contract District is RegistryEntry, MiniMeToken, StakeBank
+contract District is RegistryEntry, MiniMeToken, StakeBank, Power
+
 {
   /** 
-  * @dev IPFS hash of file that contains all data from form fields
-  */
+   * @dev IPFS hash of file that contains all data from form fields
+   */
   bytes public infoHash;
+
+
+  /**
+   * @dev reserve ratio, represented in ppm, 1-1000000
+   * 1/3 corresponds to y= multiple * x^2
+   * 1/2 corresponds to y= multiple * x
+   * 2/3 corresponds to y= multiple * x^1/2
+   * multiple will depends on contract initialization,
+   * specificallytotalAmount and poolBalance parameters
+   * we might want to add an 'initialize' function that will allow
+   * the owner to send ether to the contract and mint a given amount of tokens
+   */
+  uint32 public dntWeight;
 
   /**
    * @dev Constructor for this contract.
@@ -30,11 +45,13 @@ contract District is RegistryEntry, MiniMeToken, StakeBank
   function construct(
     address _creator,
     uint _version,
-    bytes _infoHash
+    bytes _infoHash,
+    uint32 _dntWeight
   )
   public
   {
     super.construct(_creator, _version);
+
     super.constructMiniMeToken(
       this,
       0x0,
@@ -45,16 +62,75 @@ contract District is RegistryEntry, MiniMeToken, StakeBank
       false 
     );
     changeController(this);
+
     super.constructStakeBank(registryToken);
+
+    super.constructPower();
+
     challengePeriodEnd = ~uint(0);
 
     infoHash = _infoHash;
+
+    require(_dntWeight >= 1 && _dntWeight <= MAX_WEIGHT);
+    dntWeight = _dntWeight;
   }
 
   function setInfoHash(bytes _infoHash) public
   {
     require(msg.sender == creator);
     infoHash = _infoHash;
+  }
+
+  /**
+   * @dev given a token supply, connector balance, weight and a deposit amount (in the connector token),
+   * calculates the return for a given conversion (in the main token)
+   *
+   * Formula:
+   * Return = _supply * ((1 + _depositAmount / _connectorBalance) ^ (_connectorWeight / 1000000) - 1)
+   *
+   * @param _supply              token total supply
+   * @param _connectorBalance    total connector balance
+   * @param _connectorWeight     connector weight, represented in ppm, 1-1000000
+   * @param _depositAmount       deposit amount, in connector token
+   *
+   *  @return purchase return amount
+   */
+  function calculatePurchaseReturn(
+    uint256 _supply,
+    uint256 _connectorBalance,
+    uint32 _connectorWeight,
+    uint256 _depositAmount) public constant returns (uint256)
+  {
+    // validate input
+    require(_supply > 0 && _connectorBalance > 0 && _connectorWeight > 0 && _connectorWeight <= MAX_WEIGHT);
+
+    // special case for 0 deposit amount
+    if (_depositAmount == 0) {
+      return 0;
+    }
+
+    // special case if the weight = 100%
+    if (_connectorWeight == MAX_WEIGHT) {
+      return _supply.mul(_depositAmount).div(_connectorBalance);
+    }
+
+    uint256 result;
+    uint8 precision;
+    uint256 baseN = _depositAmount.add(_connectorBalance);
+    (result, precision) = power(baseN, _connectorBalance, _connectorWeight, MAX_WEIGHT);
+    uint256 temp = _supply.mul(result) >> precision;
+    return temp - _supply;
+  }
+
+  function estimateReturnForStake(uint _amount) public view returns (uint) {
+    uint supply = 1e19;
+    uint staked = 1e14;
+    return calculatePurchaseReturn(
+      totalSupply() + supply,
+      totalStaked() + staked,
+      dntWeight,
+      _amount
+    );
   }
 
   /// @notice Stakes a certain amount of tokens for another user.
@@ -65,7 +141,7 @@ contract District is RegistryEntry, MiniMeToken, StakeBank
   public 
   {
     super.stakeFor(_user, _amount, _data);
-    require(generateTokens(_user, _amount));
+    require(generateTokens(_user, estimateReturnForStake(_amount)));
     maybeAdjustStakeDelta(_user, int(_amount));
   }
 
@@ -77,8 +153,12 @@ contract District is RegistryEntry, MiniMeToken, StakeBank
   /// @param _amount Amount of tokens to unstake.
   /// @param _data Data field used for signalling in more complex staking applications.
   function unstake(uint _amount, bytes _data) public {
+    require(_amount > 0);
+    uint staked = totalStakedFor(msg.sender);
+    uint minted = balanceOf(msg.sender);
+    uint toDestroy = minted.div(staked.div(_amount));
     super.unstake(_amount, _data);
-    require(this.destroyTokens(msg.sender, _amount));
+    require(this.destroyTokens(msg.sender, toDestroy));
     maybeAdjustStakeDelta(msg.sender, int(_amount) * -1);
   }
 
