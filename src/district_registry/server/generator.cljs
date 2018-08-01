d714bb1663b7ecae0a3a39945207fc2eb200abbd
Dom Kiva-Meyer <git@domkm.com>
2018-08-01 16:35:42 -0400

    Move dev code from Deployer to Generator

--- src/district_registry/server/generator.cljs
+++ src/district_registry/server/generator.cljs
@@ -1,19 +1,16 @@
 (ns district-registry.server.generator
   (:require
    [bignumber.core :as bn]
+   [cljs-time.core :as t]
    [cljs-web3.core :as web3]
    [cljs-web3.eth :as web3-eth]
    [cljs-web3.evm :as web3-evm]
    [cljs-web3.utils :refer [js->cljkk camel-case]]
-   [district.cljs-utils :refer [rand-str]]
-   [district.server.config :refer [config]]
-   [district.server.smart-contracts :refer [contract-address contract-call instance]]
-   [district.server.web3 :refer [web3]]
-   [district-registry.server.contract.dnt :as dank-token]
-   [district-registry.server.contract.eternal-db :as eternal-db]
    [district-registry.server.contract.district :as district]
    [district-registry.server.contract.district-factory :as district-factory]
    [district-registry.server.contract.district-registry :as district-registry]
+   [district-registry.server.contract.dnt :as dnt]
+   [district-registry.server.contract.eternal-db :as eternal-db]
    [district-registry.server.contract.minime-token :as minime-token]
    [district-registry.server.contract.param-change :as param-change]
    [district-registry.server.contract.param-change-factory :as param-change-factory]
@@ -21,6 +18,10 @@
    [district-registry.server.contract.registry :as registry]
    [district-registry.server.contract.registry-entry :as registry-entry]
    [district-registry.server.deployer]
+   [district.cljs-utils :refer [rand-str]]
+   [district.server.config :refer [config]]
+   [district.server.smart-contracts :refer [contract-address contract-call instance]]
+   [district.server.web3 :refer [web3]]
    [mount.core :as mount :refer [defstate]]))
 
 (declare start)
@@ -69,7 +70,7 @@
 
               (when-not (= :scenario/challenge scenario-type)
                 (let [{:keys [:reg-entry/creator]} (registry-entry/load-registry-entry registry-entry)
-                      balance (dank-token/balance-of creator)]
+                      balance (dnt/balance-of creator)]
 
                   (registry-entry/approve-and-commit-vote registry-entry
                     {:amount balance
@@ -118,9 +119,170 @@
 
           (param-change-registry/apply-param-change registry-entry {:from account}))))))
 
+(defn generate-district-and-challenges [_]
+  (let [accounts (web3-eth/accounts @web3)
+        district (district-factory/approve-and-create-district
+                   {:info-hash "QmZJWGiKnqhmuuUNfcryiumVHCKGvVNZWdy7xtd3XCkQJH"
+                    :dnt-weight 333333
+                    :amount (web3/to-wei 10 :ether)}
+                   {:from (first accounts)})
+        reg-entry (-> district
+                    district-registry/registry-entry-event-in-tx
+                    :args
+                    :registry-entry)]
+
+    (prn "dnt" (dnt/balance-of (first accounts)))
+    (prn "district token" (district/balance-of reg-entry (first accounts)))
+    (prn "staked dnt" (district/total-staked-for reg-entry (first accounts)))
+
+    (prn "staked"
+      (district/approve-and-stake
+        {:district reg-entry
+         :amount (web3/to-wei 10 :ether)
+         :data 0}
+        {:from (first accounts)}))
+
+    (prn "dnt" (dnt/balance-of (first accounts)))
+    (prn "district token" (district/balance-of reg-entry (first accounts)))
+    (prn "staked dnt" (district/total-staked-for reg-entry (first accounts)))
+
+    (prn "staked"
+      (district/approve-and-stake
+        {:district reg-entry
+         :amount (web3/to-wei 10 :ether)
+         :data 0}
+        {:from (first accounts)}))
+
+    (prn "dnt" (dnt/balance-of (first accounts)))
+    (prn "district token" (district/balance-of reg-entry (first accounts)))
+    (prn "staked dnt" (district/total-staked-for reg-entry (first accounts)))
+
+    (prn "unstaked"
+      (district/unstake reg-entry (web3/to-wei 1 :ether) 0))
+
+    (prn "dnt" (dnt/balance-of (first accounts)))
+    (prn "district token" (district/balance-of reg-entry (first accounts)))
+    (prn "staked dnt" (district/total-staked-for reg-entry (first accounts)))
+
+    (prn
+      "create challenge"
+      (registry-entry/approve-and-create-challenge
+        reg-entry
+        {:amount (web3/to-wei 10 :ether)
+         :meta-hash "QmZJWGiKnqhmuuUNfcryiumVHCKGvVNZWdy7xtd3XCkQJH"}
+        {:from (last accounts)}))
+
+    (prn "commit vote"
+      (registry-entry/approve-and-commit-vote
+        reg-entry
+        {:amount (web3/to-wei 1 :ether)
+         :salt "abc"
+         :vote-option :vote.option/include}
+        {:from (first accounts)}))
+
+    (prn "commit vote"
+      (registry-entry/approve-and-commit-vote
+        reg-entry
+        {:amount (web3/to-wei 2 :ether)
+         :salt "abc"
+         :vote-option :vote.option/exclude}
+        {:from (last accounts)}))
+
+    (prn "staked"
+      (district/approve-and-stake
+        {:district reg-entry
+         :amount (web3/to-wei 1 :ether)
+         :data 0}
+        {:from (last accounts)}))
+
+    (prn "increased time 2 mins"
+      (web3-evm/increase-time! @web3 [(inc (t/in-seconds (t/minutes 2)))]))
+
+    (prn "reveal vote"
+      (registry-entry/reveal-vote
+        reg-entry
+        {:vote-option :vote.option/include
+         :salt "abc"}
+        {:from (first accounts)}))
+
+    (prn "reveal vote"
+      (registry-entry/reveal-vote
+        reg-entry
+        {:vote-option :vote.option/exclude
+         :salt "abc"}
+        {:from (last accounts)}))
+
+    (prn "increased time 1 min"
+      (web3-evm/increase-time! @web3 [(inc (t/in-seconds (t/minutes 1)))]))
+
+    (prn "vote reward"
+      (registry-entry/vote-reward
+        reg-entry
+        (first accounts)))
+
+    (prn "vote reward"
+      (registry-entry/vote-reward
+        reg-entry
+        (last accounts)))
+
+    (prn "claim vote reward"
+      (registry-entry/claim-vote-reward
+        reg-entry
+        {:from (first accounts)}))
+
+    (prn "claim vote reward"
+      (registry-entry/claim-vote-reward
+        reg-entry
+        {:from (last accounts)}))
+
+    (prn
+      "create challenge 2"
+      (registry-entry/approve-and-create-challenge
+        reg-entry
+        {:amount (web3/to-wei 10 :ether)
+         :meta-hash "QmZJWGiKnqhmuuUNfcryiumVHCKGvVNZWdy7xtd3XCkQJH"}
+        {:from (last accounts)}))
+
+    (prn "commit vote"
+      (registry-entry/approve-and-commit-vote
+        reg-entry
+        {:amount (web3/to-wei 20 :ether)
+         :salt "abc"
+         :vote-option :vote.option/exclude}
+        {:from (last accounts)}))
+
+    (prn "increased time 2 mins"
+      (web3-evm/increase-time! @web3 [(inc (t/in-seconds (t/minutes 2)))]))
+
+    (prn "reveal vote"
+      (registry-entry/reveal-vote
+        reg-entry
+        {:vote-option :vote.option/exclude
+         :salt "abc"}
+        {:from (last accounts)}))
+
+    (prn "increased time 1 min"
+      (web3-evm/increase-time! @web3 [(inc (t/in-seconds (t/minutes 1)))]))
+
+    (prn "vote reward"
+      (registry-entry/vote-reward
+        reg-entry
+        (last accounts)))
+
+    (prn "claim vote reward"
+      (registry-entry/claim-vote-reward
+        reg-entry
+        {:from (last accounts)}))
+
+    (prn "claim challenge reward"
+      (registry-entry/claim-challenge-reward
+        reg-entry
+        {:from (last accounts)}))))
 
 (defn start [opts]
   (let [opts (assoc opts :accounts (web3-eth/accounts @web3))]
+    ;; TODO: Parameterize district creation and remove printing
+    (generate-district-and-challenges opts)
     #_
     (generate-districts opts)
     #_
