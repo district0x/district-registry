6ef5411add4713cee4e934391cb66425953276c1
Dom Kiva-Meyer <git@domkm.com>
2019-04-01 11:15:54 -0400

    Fix compilation

--- src/district_registry/server/generator.cljs
+++ src/district_registry/server/generator.cljs
@@ -47,84 +47,6 @@
           scenarios-repeated (take (* use-accounts items-per-account) (cycle scenarios))]
       (partition 2 (interleave accounts-repeated scenarios-repeated)))))
 
-(defn generate-districts [{:keys [:accounts :districts/use-accounts :districts/items-per-account :districts/scenarios]}]
-  (let [[max-total-supply max-auction-duration deposit commit-period-duration reveal-period-duration]
-        (->> (eternal-db/get-uint-values :district-registry-db [:max-total-supply :max-auction-duration :deposit :commit-period-duration
-                                                                :reveal-period-duration])
-          (map bn/number))]
-    (doseq [[account {:keys [:scenario-type]}] (get-scenarios {:accounts accounts
-                                                               :use-accounts use-accounts
-                                                               :items-per-account items-per-account
-                                                               :scenarios scenarios})]
-      (let [meta-hash "QmZJWGiKnqhmuuUNfcryiumVHCKGvVNZWdy7xtd3XCkQJH"
-            total-supply (inc (rand-int max-total-supply))
-            auction-duration (+ 60 (rand-int (- max-auction-duration 60)))]
-
-        (let [tx-hash (district-factory/approve-and-create-district {:meta-hash meta-hash
-                                                                     :total-supply total-supply
-                                                                     :amount deposit}
-                        {:from account})]
-
-          (when-not (= :scenario/create scenario-type)
-            (let [{{:keys [:registry-entry]} :args} (district-registry/registry-entry-event-in-tx tx-hash)]
-              (when-not registry-entry
-                (throw (js/Error. "Registry Entry wasn't found")))
-
-              (registry-entry/approve-and-create-challenge registry-entry
-                {:meta-hash meta-hash
-                 :amount deposit}
-                {:from account})
-
-              (when-not (= :scenario/challenge scenario-type)
-                (let [{:keys [:reg-entry/creator]} (registry-entry/load-registry-entry registry-entry)
-                      balance (dnt/balance-of creator)]
-
-                  (registry-entry/approve-and-commit-vote registry-entry
-                    {:amount balance
-                     :salt "abc"
-                     :vote-option :vote.option/vote-for}
-                    {:from creator})
-
-                  (when-not (= :scenario/commit-votde scenario-type)
-                    (web3-evm/increase-time! @web3 [(inc commit-period-duration)])
-
-                    (registry-entry/reveal-vote registry-entry
-                      {:vote-option :vote.option/vote-for
-                       :salt "abc"}
-                      {:from creator})
-
-                    (when-not (= :scenario/reveal-vote scenario-type)
-                      (web3-evm/increase-time! @web3 [(inc reveal-period-duration)])
-
-                      (registry-entry/claim-vote-reward registry-entry {:from creator}))))))))))))
-
-(defn generate-param-changes [{:keys [:accounts
-                                      :param-changes/use-accounts
-                                      :param-changes/items-per-account
-                                      :param-changes/scenarios]}]
-  (let [[deposit challenge-period-duration]
-        (->> (eternal-db/get-uint-values :param-change-registry-db [:deposit :challenge-period-duration])
-          (map bn/number))]
-    (doseq [[account {:keys [:scenario-type :param-change-db]}]
-            (get-scenarios {:accounts accounts
-                            :use-accounts use-accounts
-                            :items-per-account items-per-account
-                            :scenarios scenarios})]
-
-      (let [tx-hash (param-change-factory/approve-and-create-param-change
-                      {:db (contract-address (or param-change-db :district-registry-db))
-                       :key :deposit
-                       :value (web3/to-wei 800 :ether)
-                       :amount deposit}
-                      {:from account})
-
-            {:keys [:registry-entry]} (:args (param-change-registry/registry-entry-event-in-tx tx-hash))]
-
-        (when-not (= scenario-type :scenario/create)
-          (web3-evm/increase-time! @web3 [(inc challenge-period-duration)])
-
-          (param-change-registry/apply-param-change registry-entry {:from account}))))))
-
 (defn upload-image [file-path]
   (log/info "Uploading" file-path ::upload-image)
   (js/Promise.
@@ -155,19 +77,7 @@
             (do
               (log/info "Uploaded data received " {:hash hash} ::upload-data)
               (resolve hash))))))))
-#_
-(prn
-  (contract-event-in-tx
-    "0xda687280ce3a1bc8a3c5633a3d10220f8b29192381e3607f5affc952f7323546"
-    :district-registry
-    :DistrictConstructedEvent
-    ))
-#_
-(district-factory/approve-and-create-district
-  {:meta-hash "QmbgUT4MgkHuJoTJspTNwMPBNyNRp96MxqHwhfacF4rRgV"
-   :dnt-weight 333333
-   :amount (web3/to-wei 10000 :wei)}
-  {:from "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"})
+
 (defn generate-district-and-challenges [{:keys [district-hashes
                                                 challenge-hashes]}]
   (prn "district hashes" district-hashes)
