9b4a0986bc22ed922217c09b025eb9b1765d2e60
Dom Kiva-Meyer <git@domkm.com>
2019-04-01 11:15:54 -0400

    Fix compilation

--- src/district_registry/server/contract/district_registry.cljs
+++ src/district_registry/server/contract/district_registry.cljs
@@ -1,7 +1,2 @@
 (ns district-registry.server.contract.district-registry
   (:require [district-registry.server.contract.registry :as registry]))
-
-(def registry-entry-event (partial registry/registry-entry-event [:district-registry :district-registry-fwd]))
-(def registry-entry-event-in-tx (partial registry/registry-entry-event-in-tx [:district-registry :district-registry-fwd]))
-
-
