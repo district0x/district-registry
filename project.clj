30240bc4a68e1516d82a1c8ea48080b11732c099
Dom Kiva-Meyer <git@domkm.com>
2019-04-05 12:22:56 -0400

    Fix submit form image upload button styling and display styled errors Resolves #16 Resolves #25

--- project.clj
+++ project.clj
@@ -30,7 +30,7 @@
                  [district0x/district-server-web3-watcher "1.0.2"]
                  [district0x/district-ui-component-active-account "1.0.0"]
                  [district0x/district-ui-component-active-account-balance "1.0.1"]
-                 [district0x/district-ui-component-form "0.1.11-SNAPSHOT"]
+                 [district0x/district-ui-component-form "0.2.6"]
                  [district0x/district-ui-component-notification "1.0.0"]
                  [district0x/district-ui-component-tx-button "1.0.0"]
                  [district0x/district-ui-graphql "1.0.7"]
--- src/district_registry/ui/submit/page.cljs
+++ src/district_registry/ui/submit/page.cljs
@@ -30,7 +30,7 @@
 
 (defmethod page :route/submit []
   (let [deposit-query (subscribe [::gql/query {:queries [(param-search-query :deposit)]}])
-        form-data (r/atom {})
+        form-data (r/atom {:dnt-weight 1000000})
         dnt-weight-on-change (fn [weight]
                                #(swap! form-data assoc :dnt-weight weight))
         errors (reaction {:local (let [{:keys [name
@@ -41,11 +41,12 @@
                                                background-file-info
                                                dnt-weight]} @form-data]
                                    (cond-> {}
+
                                      (empty? name)
-                                     (assoc-in [:name :error] "District title is mandatory")
+                                     (assoc-in [:name :error] "District title is required")
 
                                      (empty? description)
-                                     (assoc-in [:description :error] "District description is mandatory")
+                                     (assoc-in [:description :error] "District description is required")
 
                                      (and
                                        (seq url)
@@ -60,127 +61,124 @@
                                      (not logo-file-info) (assoc-in [:logo-file-info :error] "No logo file selected")
                                      (not background-file-info) (assoc-in [:background-file-info :error] "No background file selected")))})]
     (fn []
-      (let []
-        [app-layout
-         [:section#main
-          [:div.container
-           [:div.box-wrap
-            [:div.hero
-             [:div.container
-              [:div.header-image.sized
-               [:img {:src "/images/submit-header@2x.png"}]]
-              [:div.page-title [:h1 "Submit"]]
-              ]]
-            [:div.body-text
-             [:div.container
-              [:p.intro-text
-               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a augue quis metus sollicudin mattis. Duis efficitur tellus felis, et tincidunt turpis aliquet non. Aenean augue metus, masuada non rutrum ut, ornare ac orci. Lorem ipsum dolor sit amet, consectetur adipiscing. Lorem augue quis metus sollicitudin mattis. Duis efficitur tellus felis, et tincidunt turpis aliquet non."]
-              [:ul
-               (->> @errors
-                 :local
-                 (map (fn [[k v]]
-                        [:li {:key k} (:error v)]))
-                 doall)]
-              [:form.image-upload
-               [:div.row.spaced
-                [:div.col.left
-                 [text-input {:form-data form-data
-                              :placeholder "Name"
-                              :errors errors
-                              :id :name}]
-                 [text-input {:form-data form-data
-                              :placeholder "URL"
-                              :errors errors
-                              :id :url}]
-                 [text-input {:form-data form-data
-                              :placeholder "GitHub URL"
-                              :errors errors
-                              :id :github-url}]]
-                [:div.col.right
-                 [textarea-input {:form-data form-data
-                                  :placeholder "Description"
-                                  :errors errors
-                                  :id :description}]]]
-               [:div.form-btns
-                [:div.btn-wrap
-                 [:button.cta-btn.hasIcon
-                  [:span.icon.icon-upload]
-                  [file-drag-input {:form-data form-data
-                                    :id :logo-file-info
-                                    :errors errors
-                                    :label "Upload Logo"
-                                    :file-accept-pred (fn [{:keys [name type size] :as props}]
-                                                        (or
-                                                          (= type "image/png")
-                                                          (= type "image/jpg")
-                                                          (= type "image/jpeg")))
-                                    :on-file-accepted (fn [{:keys [name type size array-buffer] :as props}]
-                                                        (prn "Accepted " props))
-                                    :on-file-rejected (fn [{:keys [name type size] :as props}]
-                                                        (prn "Rejected " props))}]]
-                 [:p "Size 256 x 256"]]
-                [:div.btn-wrap
-                 [:button.cta-btn.hasIcon
-                  [:span.icon.icon-upload]
-                  [file-drag-input {:form-data form-data
-                                    :id :background-file-info
-                                    :errors errors
-                                    :label "Upload a background"
-                                    :file-accept-pred (fn [{:keys [name type size] :as props}]
-                                                        (or
-                                                          (= type "image/png")
-                                                          (= type "image/jpg") ))
-                                    :on-file-accepted (fn [{:keys [name type size array-buffer] :as props}]
-                                                        (prn "Accepted " props))
-                                    :on-file-rejected (fn [{:keys [name type size] :as props}]
-                                                        (prn "Rejected " props))}]]
-                 [:p "Size 1024 x 325"]]]]
-              [:div.h-line]
-              [:h2 "Voting Token Issuance Curve"]
-              [:form.voting
-               [:div.radio-boxes
-
-                [:div.radio-box
-                 [:fieldset
-                  [:input#r1 {:name "radio-group", :type "radio"
-                              :on-change (dnt-weight-on-change 333333)}]
-                  [:label {:for "r1"} "Curve Option 1/3"]]
-                 [:img.radio-img {:src "/images/curve-graph-333333-m.png"}]
-                 [:p
-                  "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]
-                [:div.radio-box
-                 [:fieldset
-                  [:input#r2 {:name "radio-group", :type "radio"
-                              :on-change (dnt-weight-on-change 500000)}]
-                  [:label {:for "r2"} "Curve Option 1/2"]]
-                 [:img.radio-img {:src "/images/curve-graph-500000-m.png"}]
-                 [:p
-                  "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]
-                [:div.radio-box
-                 [:fieldset
-                  [:input#r3 {:name "radio-group", :type "radio"
-                              :on-change (dnt-weight-on-change 1000000)}]
-                  [:label {:for "r3"} "Curve Option 1/1"]]
-                 [:img.radio-img {:src "/images/curve-graph-1000000-m.png"}]
-                 [:p
-                  "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]]
-               [:div.form-btns
-                  [:p (-> @deposit-query
-                        :search-param-changes
-                        :items
-                        first
-                        :param-change/value
-                        (web3/from-wei :ether)
-                        format/format-dnt)]
-                [:button.cta-btn
-                   {:on-click (fn [e]
-                                (.preventDefault e)
-                                (when-not (-> @errors :local seq)
-                                  (dispatch [::events/add-district-logo (->> @deposit-query
-                                                                          :search-param-changes
-                                                                          :items
-                                                                          first
-                                                                          :param-change/value
-                                                                          (assoc @form-data :deposit))])))
-                    :type "submit"}
-                 "Submit"]]]]]]]]]))))
+      (prn @errors)
+      (prn @form-data)
+      [app-layout
+       [:section#main
+        [:div.container
+         [:div.box-wrap
+          [:div.hero
+           [:div.container
+            [:div.header-image.sized
+             [:img {:src "/images/submit-header@2x.png"}]]
+            [:div.page-title [:h1 "Submit"]]
+            ]]
+          [:div.body-text
+           [:div.container
+            [:p.intro-text
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a augue quis metus sollicudin mattis. Duis efficitur tellus felis, et tincidunt turpis aliquet non. Aenean augue metus, masuada non rutrum ut, ornare ac orci. Lorem ipsum dolor sit amet, consectetur adipiscing. Lorem augue quis metus sollicitudin mattis. Duis efficitur tellus felis, et tincidunt turpis aliquet non."]
+            [:form.image-upload
+             [:div.row.spaced
+              [:div.col.left
+               [text-input {:form-data form-data
+                            :placeholder "Name"
+                            :errors errors
+                            :id :name}]
+               [text-input {:form-data form-data
+                            :placeholder "URL"
+                            :errors errors
+                            :id :url}]
+               [text-input {:form-data form-data
+                            :placeholder "GitHub URL"
+                            :errors errors
+                            :id :github-url}]]
+              [:div.col.right
+               [textarea-input {:form-data form-data
+                                :placeholder "Description"
+                                :errors errors
+                                :id :description}]]]
+             [:div.form-btns
+              [:div.btn-wrap
+               [file-drag-input {:form-data form-data
+                                 :id :logo-file-info
+                                 :errors errors
+                                 :label "Upload Logo"
+                                 :file-accept-pred (fn [{:keys [name type size] :as props}]
+                                                     (or
+                                                       (= type "image/png")
+                                                       (= type "image/jpg")
+                                                       (= type "image/jpeg")))
+                                 :on-file-accepted (fn [{:keys [name type size array-buffer] :as props}]
+                                                     (prn "Accepted " props))
+                                 :on-file-rejected (fn [{:keys [name type size] :as props}]
+                                                     (prn "Rejected " props))}]
+               [:p "Size 256 x 256"]]
+              [:div.btn-wrap
+               [file-drag-input {:form-data form-data
+                                 :id :background-file-info
+                                 :errors errors
+                                 :label "Upload Background"
+                                 :file-accept-pred (fn [{:keys [name type size] :as props}]
+                                                     (or
+                                                       (= type "image/png")
+                                                       (= type "image/jpg") ))
+                                 :on-file-accepted (fn [{:keys [name type size array-buffer] :as props}]
+                                                     (prn "Accepted " props))
+                                 :on-file-rejected (fn [{:keys [name type size] :as props}]
+                                                     (prn "Rejected " props))}]
+               [:p "Size 1024 x 325"]]]]
+            [:div.h-line]
+            [:h2 "Voting Token Issuance Curve"]
+            [:form.voting
+             [:div.radio-boxes
+              [:div.radio-box
+               [:fieldset
+                [:input#r1 {:name "radio-group"
+                            :type "radio"
+                            :checked (= (:dnt-weight @form-data) 333333)
+                            :on-change (dnt-weight-on-change 333333)}]
+                [:label {:for "r1"} "Curve Option 1/3"]]
+               [:img.radio-img {:src "/images/curve-graph-333333-m.png"}]
+               [:p
+                "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]
+              [:div.radio-box
+               [:fieldset
+                [:input#r2 {:name "radio-group"
+                            :type "radio"
+                            :checked (= (:dnt-weight @form-data) 500000)
+                            :on-change (dnt-weight-on-change 500000)}]
+                [:label {:for "r2"} "Curve Option 1/2"]]
+               [:img.radio-img {:src "/images/curve-graph-500000-m.png"}]
+               [:p
+                "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]
+              [:div.radio-box
+               [:fieldset
+                [:input#r3 {:name "radio-group"
+                            :type "radio"
+                            :checked (= (:dnt-weight @form-data) 1000000)
+                            :on-change (dnt-weight-on-change 1000000)}]
+                [:label {:for "r3"} "Curve Option 1/1"]]
+               [:img.radio-img {:src "/images/curve-graph-1000000-m.png"}]
+               [:p
+                "Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do eiusmod."]]]
+             [:div.form-btns
+              [:p (-> @deposit-query
+                    :search-param-changes
+                    :items
+                    first
+                    :param-change/value
+                    (web3/from-wei :ether)
+                    format/format-dnt)]
+              [:button
+               {:disabled (-> @errors :local seq boolean)
+                :on-click (fn [e]
+                            (.preventDefault e)
+                            (when-not (-> @errors :local seq)
+                              (dispatch [::events/add-district-logo (->> @deposit-query
+                                                                      :search-param-changes
+                                                                      :items
+                                                                      first
+                                                                      :param-change/value
+                                                                      (assoc @form-data :deposit))])))
+                :type "submit"}
+               "Submit"]]]]]]]]])))
