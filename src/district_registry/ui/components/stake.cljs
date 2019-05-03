f0cc0baa0afd55967b1556a275b605115072ec5c
Dom Kiva-Meyer <dom@paren.com>
2019-05-03 15:11:06 -0400

    Disable stake and unstake buttons when the user hasn't staked or doesn't have DNT respectively Resolves #46

--- src/district_registry/ui/components/stake.cljs
+++ src/district_registry/ui/components/stake.cljs
@@ -10,9 +10,19 @@
    [re-frame.core :refer [subscribe dispatch]]
    [reagent.core :as r]))
 
+(defn ^:private gql-stake-info [district-address]
+  (let [active-account (subscribe [::account-subs/active-account])]
+    (subscribe [::gql/query
+                {:queries [[:district {:reg-entry/address district-address}
+                            [:reg-entry/address
+                             :district/total-supply
+                             [:district/dnt-staked-for {:staker @active-account}]
+                             [:district/balance-of {:staker @active-account}]]]]}
+                {:refetch-on #{::district/approve-and-stake-for-success
+                               ::district/unstake-success}}])))
+
 (defn stake-form [district-address]
-  (let [address district-address
-        input (r/atom "1")
+  (let [input (r/atom "1")
         on-change (fn [e]
                     (let [s (-> e .-target .-value)
                           i (js/parseInt s)]
@@ -20,25 +30,34 @@
                             (and
                               (integer? i)
                               (pos? i)))
-                        (reset! input s))))]
-    (fn [address]
+                        (reset! input s))))
+        query (gql-stake-info district-address)
+        dnt-balance (subscribe [:district.ui.web3-account-balances.subs/active-account-balance :DNT])]
+    (fn [district-address]
       [:div.box-cta.stake-form
        [:form
         [:div.form-btns
          [:div.cta-btns
-          [:a.cta-btn {:href "#"
-                       :on-click (fn []
-                                   (dispatch [::district/approve-and-stake-for {:address address
-                                                                                :dnt (-> @input
-                                                                                       web3/to-big-number
-                                                                                       (web3/to-wei :ether))}]))}
+          [:button.cta-btn {:disabled (not (and
+                                             (bn/bignumber? @dnt-balance)
+                                             (bn/< 0 @dnt-balance)))
+                            :on-click (fn [e]
+                                        (.preventDefault e)
+                                        (dispatch [::district/approve-and-stake-for {:address district-address
+                                                                                     :dnt (-> @input
+                                                                                            web3/to-big-number
+                                                                                            (web3/to-wei :ether))}]))}
            "Stake"]
-          [:a.cta-btn {:href "#"
-                       :on-click (fn []
-                                   (dispatch [::district/unstake {:address address
-                                                                  :dnt (-> @input
-                                                                         web3/to-big-number
-                                                                         (web3/to-wei :ether))}]))}
+          [:button.cta-btn {:disabled (->> @query
+                                        :district
+                                        :district/dnt-staked-for
+                                        not)
+                            :on-click (fn [e]
+                                        (.preventDefault e)
+                                        (dispatch [::district/unstake {:address district-address
+                                                                       :dnt (-> @input
+                                                                              web3/to-big-number
+                                                                              (web3/to-wei :ether))}]))}
            "Unstake"]]
          [:fieldset
           [:input.stake-input
@@ -48,15 +67,7 @@
           [:span.cur "DNT"]]]]])))
 
 (defn stake-info [district-address]
-  (let [active-account (subscribe [::account-subs/active-account])
-        query (subscribe [::gql/query
-                          {:queries [[:district {:reg-entry/address district-address}
-                                      [:reg-entry/address
-                                       :district/total-supply
-                                       [:district/dnt-staked-for {:staker @active-account}]
-                                       [:district/balance-of {:staker @active-account}]]]]}
-                          {:refetch-on #{::district/approve-and-stake-for-success
-                                         ::district/unstake-success}}])]
+  (let [query (gql-stake-info district-address)]
     (fn []
       (when-not (:graphql/loading? @query)
         (let [{:as district
