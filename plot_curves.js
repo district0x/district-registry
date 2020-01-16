e43cae489587bdf0a5a92ea1e24553d2d31a4553
filip <fbielejec@gmail.com>
2020-01-16 11:02:39 +0100

    async/await

--- plot_curves.js
+++ plot_curves.js
@@ -14,7 +14,7 @@
 // contractInstance.methods.MAX_WEIGHT().call();
 
 const fs = require ('fs');
-const StakeBank = artifacts.require("StakeBank");
+// const StakeBank = artifacts.require("StakeBank");
 const contractAddress = "0xc2de159ca5175da01a972c3a7e41ec0579a62948";
 
 // -- PARAMS-- //
@@ -25,11 +25,13 @@ const init_tokenSupply = 1000;
 const init_connectorBalance = 100;
 
 // npx truffle exec ./plot_curves.js --network ganache
-module.exports = function(callback) {
+module.exports = async function(callback) {
 
   console.log ("@@@ using Web3 version:", web3.version);
 
-  const stakeBank = StakeBank.at (contractAddress);
+  var abi = JSON.parse(fs.readFileSync('./resources/public/contracts/build/StakeBank.json')).abi;
+  const stakeBank = new web3.eth.Contract(abi, contractAddress);
+  // const stakeBank = StakeBank.at (contractAddress);
 
   var n = 100;
   var tokenSupply = new Array(n).fill(null);
@@ -49,24 +51,22 @@ module.exports = function(callback) {
     var currentTokenSupply = tokenSupply [i];
     var currentConnectorBalance = connectorBalance [i];
 
+    console.log ("@@@ ARGS", currentTokenSupply);
+
     // returns the amount of continuous token you get for the depositAmount of connector token
-    stakeBank.methods.calculatePurchaseReturn(currentTokenSupply, currentConnectorBalance, connectorWeight, depositAmount).call({from: '0x4c3F13898913F15F12F902d6480178484063A6Fb'}).then( (result) => {
+    var result = await stakeBank.methods.calculatePurchaseReturn(currentTokenSupply, currentConnectorBalance, connectorWeight, depositAmount).call({from: '0x4c3F13898913F15F12F902d6480178484063A6Fb'});
 
-      // price is denominated in connector tokens
-      var currentPrice = depositAmount / result;
-      console.log ("You receive " + result + " continuous tokens for the price of" + currentPrice);
+    console.log ("@@@ RES", result);
 
-      tokenSupply [i + 1] = result;
-      connectorBalance [i + 1] = depositAmount;
-      tokenPrice [i + 1] = currentPrice;
+    // price is denominated in connector tokens
+    var currentPrice = depositAmount / result;
+    console.log ("You receive " + result + " continuous tokens for the price of " + currentPrice);
 
-    } );
+    tokenSupply [i + 1] = result;
+    connectorBalance [i + 1] = depositAmount;
+    tokenPrice [i + 1] = currentPrice;
 
   }
-
-
-
-
 
   callback ();
 }
