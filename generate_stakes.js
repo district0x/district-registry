6b006a203a4e623e0c34def477b70c0f98c9606a
filip <fbielejec@gmail.com>
2020-01-22 14:09:22 +0100

    create district directly in the contract

--- generate_stakes.js
+++ generate_stakes.js
@@ -1,72 +1,94 @@
-const {copy, smartContractsTemplate, encodeContractEDN, linkBytecode, readSmartContractsFile, writeSmartContracts, getSmartContractAddress, setSmartContractAddress, copyContract} = require("./migrations/utils.js");
-const {env, smartContractsPath, parameters} = require("./truffle.js");
+const { copy, smartContractsTemplate, encodeContractEDN, linkBytecode, readSmartContractsFile, writeSmartContracts, getSmartContractAddress, setSmartContractAddress, copyContract } = require("./migrations/utils.js");
+const { env, smartContractsPath, parameters } = require("./truffle.js");
 const fs = require ('fs');
 const BN = require('bn.js');
 
-function requireContract(contractName, contractCopyName) {
-  return artifacts.require(copyContract(contractName, contractCopyName));
-}
-
-let DNT = requireContract("District0xNetworkToken");
-let DistrictFactory = requireContract("DistrictFactory");
+const smartContracts = readSmartContractsFile(smartContractsPath);
 
 // -- PARAMS-- //
 
+const firstAccountAddress = '0x4c3F13898913F15F12F902d6480178484063A6Fb';
+const MAX_WEIGHT = 1000000;
+// const connectorWeight =  333333;
+const connectorWeight =  500000;
+//const connectorWeight =  MAX_WEIGHT;
+
+async function contractEventInTx (contract, event, transactionHash) {
+  var txReceipt = await web3.eth.getTransactionReceipt(transactionHash);
+  var eventInterface = contract._jsonInterface.filter(element => element.name == event) [0];
+  var eventLog = txReceipt.logs.filter (element => element.topics [0] == eventInterface.signature) [0];
+
+  return web3.eth.abi.decodeLog(eventInterface.inputs, eventLog.data, eventLog.topics);
+}
+
 // npx truffle exec ./generate_stakes.js --network ganache
 module.exports = async function(callback) {
+  try {
 
-  var smartContracts = readSmartContractsFile(smartContractsPath);
+    const registry = new web3.eth.Contract(JSON.parse(fs.readFileSync('./resources/public/contracts/build/Registry.json')).abi,
+                                           getSmartContractAddress(smartContracts, ":district-registry-fwd"));
 
-  var dnt = await DNT.at(getSmartContractAddress(smartContracts, ":DNT"));
+    const dnt = new web3.eth.Contract(JSON.parse(fs.readFileSync('./resources/public/contracts/build/District0xNetworkToken.json')).abi,
+                                      getSmartContractAddress(smartContracts, ":DNT"));
 
-  var districtFactory = await DistrictFactory.at(getSmartContractAddress(smartContracts, ":district-factory"));
+    const districtFactory = new web3.eth.Contract(JSON.parse(fs.readFileSync('./resources/public/contracts/build/DistrictFactory.json')).abi,
+                                                  getSmartContractAddress(smartContracts, ":district-factory"));
 
-  // console.log ("@@@ using Web3 version:", web3.version);
-  var abi = JSON.parse(fs.readFileSync('./resources/public/contracts/build/District.json')).abi;
+    const districtId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
 
-  var firstAccountAddress='0x4c3F13898913F15F12F902d6480178484063A6Fb';
-  const districtAddress="0x6652ab7df38d53220bd902c0d91378fa437ce858";
-  const district = new web3.eth.Contract(abi, districtAddress);
+    var extraData = districtFactory.methods.createDistrict(firstAccountAddress, "0x0", web3.utils.toHex(connectorWeight), districtId).encodeABI();
 
-  var depositAmount;// = new BN(web3.utils.toWei(String(Math.floor(Math.random() * 20) + 1), "ether"));
+    var createTx = await dnt.methods.approveAndCall(districtFactory._address,
+                                                    web3.utils.toHex(parameters.districtRegistryDb.deposit),
+                                                    extraData)
+        .send ({from: firstAccountAddress, gas: 10000000});
 
-  var n = 100;
-  var i;
-  var staked=new BN("0");
+    var districtAddress = (await contractEventInTx (registry, "DistrictConstructedEvent", createTx.transactionHash)).registryEntry;
 
-  for (i = 0; i < n; i++) {
-    try {
+    console.log("DEBUG: " +  districtAddress);
+
+    const district = new web3.eth.Contract(JSON.parse(fs.readFileSync('./resources/public/contracts/build/District.json')).abi, districtAddress);
+
+    var n = 0;
+    let i;
+    let staked = new BN("0");
+
+    for (i = 0; i < n; i++) {
 
       console.log("Currently staked: " + staked);
       var isStake = Math.random() >= 0.5;
 
-
-
       if (i == 0 || isStake == true) {
 
-        depositAmount = new BN(web3.utils.toWei(String(Math.floor(Math.random() * 20) + 1), "ether"));
+        var depositAmount = new BN(web3.utils.toWei(String(Math.floor(Math.random() * 20) + 1), "ether"));
+
+        console.log("DEBUG: " + depositAmount);
 
         var extraData = await district.methods.stakeFor(firstAccountAddress, web3.utils.toHex(depositAmount)).encodeABI();
 
-        stakeTx = await dnt.approveAndCall(districtAddress, depositAmount, extraData, {from: firstAccountAddress, gas: 10000000});
+        console.log("DEBUG: " + JSON.stringify (extraData));
 
-        staked=staked.add(depositAmount);
+        stakeTx = await dnt.methods.approveAndCall(districtAddress, depositAmount, extraData)
+          .send ({from: firstAccountAddress, gas: 10000000});
+
+        staked = staked.add(depositAmount);
         console.log("Stake tx: " + stakeTx);
+
       } else {
 
-        depositAmount = new BN(web3.utils.toWei(String(BN.min(staked , Math.floor(Math.random() * 20) + 1)), "ether"));
-        console.log("Unstaking " + depositAmount);
+        var unstakeAmount = new BN(web3.utils.toWei(String(BN.min(staked, Math.floor(Math.random() * 20) + 1)), "ether"));
+        console.log("Unstaking " + unstakeAmount);
 
-        unstakeTx = await district.methods.unstake (web3.utils.toHex (depositAmount)).send ({from: firstAccountAddress, gas: 10000000});
+        unstakeTx = await district.methods.unstake (web3.utils.toHex (unstakeAmount)).send ({from: firstAccountAddress, gas: 10000000});
 
-        staked=staked.sub(depositAmount);
+        staked = staked.sub(unstakeAmount);
         console.log("Unstake tx: " + unstakeTx);
       }
 
-    } catch (e) {
-      console.log(e);
     }
 
+  } catch (e) {
+    console.log(e);
   }
 
   callback ();
